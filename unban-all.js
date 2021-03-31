623033ff558a98de52c590c58841c797c0afb111
nano-ck <arnauld.chartierkastler@gmail.com>
2021-03-31 17:36:34 +0200

    Updated unban

--- unban-all.js
+++ unban-all.js
@@ -1,5 +1,11 @@
+/*
+Script made by NANO#1039 || Do not copy without the credits || nano-ck.cf
+
+Must be running with node js
+Must have the discord.js module installed || npm install discord.js
+*/
 const token = "ENTER YOUR BOT TOKEN HERE"
-const Discord = require('discord.js')  // Make sure you installed node's module : discord.js // npm install discord.js
+const Discord = require('discord.js')
 const client = new Discord.Client()
 const prefix = "!" // Change this to any prefix you want
 client.login(token)
@@ -23,20 +29,27 @@ client.on("message", async message => {
             message.guild.fetchBans().then(bans => {
                 if (bans.size == 0) {message.channel.send({embed:{
                         title: ":white_check_mark: All members are unbanned!",
-                        description: "Thank you! I finally unbanned all the members.",
+                        description: "Thank you! All the members are unbanned!",
                         color: 4301312,
                         footer: {
                           text: "Powered by NANO | nano-ck.cf"
                         }
                       }
-                  }); throw "No members to unban."};
+                  })};
                 bans.forEach(ban => {
-                    message.channel.send("Unbanned "+ban.user.tag)
                     message.guild.members.unban(ban.user.id).catch(function(error){
                         message.channel.send("Attempt to unban "+ban.user.tag+" failed")
                     })
                 });
-            }).then(() => message.channel.send("finished")).catch(e => console.log(e))
+            }).then(() => message.channel.send({embed:{
+                title: ":white_check_mark: All members are unbanned!",
+                description: "I just finished unbanning all the members",
+                color: 4301312,
+                footer: {
+                  text: "Powered by NANO | nano-ck.cf"
+                }
+              }
+          })).catch(e => console.log(e))
         } else {message.reply("You do not have enough permissions for this command.")}
     }
     message.channel.send("unknown command")
